1a3a532fb164d603a40d17fb31e4cb0b913d6d8c
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-04-27 22:16:52 +0200

    clean code

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,65 +1,56 @@
 var fs = require('fs'),
-    formidable = require('formidable'),
-    http = require('http'),
-    express = require('express'),
-    app = express();
-
+  formidable = require('formidable'),
+  http = require('http'),
+  express = require('express'),
+  app = express();
 
 let fileUploaded = new Object();
 
 exports.welcome = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania welcome.");
-    fs.readFile('templates/start.html', function(err, html) {
-        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-        response.write(html);
-        response.end();
-    });
-    console.log('Kończę obsługę żądania welcome.');
-}
+  console.log('Rozpoczynam obsługę żądania welcome.');
+  fs.readFile('templates/start.html', function(err, html) {
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    response.write(html);
+    response.end();
+  });
+  console.log('Kończę obsługę żądania welcome.');
+};
 
 exports.upload = function(request, response, next) {
-    console.log("Rozpoczynam obsługę żądania upload.");
-    var form = new formidable.IncomingForm();
-        form.parse(request, function(error, fields, files) {
-            if (files.upload) {
-                fileUploaded.temp = fs.readFileSync(files.upload.path);
-                fileUploaded.name = fields.title || files.upload.name;
-                fileUploaded.path = './' + fileUploaded.name;
-                fs.writeFileSync(fileUploaded.path, fileUploaded.temp);
-                fs.readFile('templates/uploaded.html', function(err, html) {
-                    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-                    response.write(html);
-                    response.end();
-                });
-                console.log("Kończę obsługę żądania upload.");
-            } else {
-                console.log('nic nie przesłano');
-                response.redirect(303, '/');
-                console.log('Kończę obsługę żądania upload bez danych.');
-            }
-        });
-
-}
+  console.log('Rozpoczynam obsługę żądania upload.');
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(error, fields, files) {
+    if (files.upload) {
+      fileUploaded.temp = fs.readFileSync(files.upload.path);
+      fileUploaded.name = fields.title || files.upload.name;
+      fileUploaded.path = './' + fileUploaded.name;
+      fs.writeFileSync(fileUploaded.path, fileUploaded.temp);
+      fs.readFile('templates/uploaded.html', function(err, html) {
+        response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        response.write(html);
+        response.end();
+      });
+      console.log('Kończę obsługę żądania upload.');
+    } else {
+      console.log('nic nie przesłano');
+      response.redirect(303, '/');
+      console.log('Kończę obsługę żądania upload bez danych.');
+    }
+  });
+};
 
 exports.show = function(request, response) {
-    fs.readFile(fileUploaded.path, "binary", function(error, file) {
-        response.writeHead(200, {"Content-Type": "image/png"});
-        response.write(file, "binary");
-        response.end();
-    });
-}
-
-/* exports.style = function(request, response) {
-    console.log('Wysyłam style.');
-    fs.readFile('./templates/style.css', "binary", function(error, file) {
-        response.writeHead(200, {"Content-Type": "text/css"});
-        response.write(file, "binary");
-        response.end();
-    });
-} */
+  console.log('Rozpoczynam obsługę żądania show.');
+  fs.readFile(fileUploaded.path, 'binary', function(error, file) {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.write(file, 'binary');
+    response.end();
+  });
+  console.log('Kończę obsługę żądania show.');
+};
 
 exports.error = function(request, response) {
-    console.log("Nie wiem co robić.");
-    response.write("404 :(");
-    response.end();
-}
+  console.log('Nie wiem co robić.');
+  response.write('404 :(');
+  response.end();
+};
--- modules/server.js
+++ modules/server.js
@@ -1,54 +1,28 @@
 var http = require('http'),
-    colors = require('colors'),
-    handlers = require('./handlers'),
-    express = require('express'),
-    app = express(),
-    port = 3000;
-    
+  colors = require('colors'),
+  handlers = require('./handlers'),
+  express = require('express'),
+  app = express(),
+  port = 3000;
+
 function start() {
-    app.all('/', function (request, response) {
-        handlers.welcome(request, response);
-    });
-    app.all('/upload', function (request, response, next) {
-        handlers.upload(request, response, next);
-    });
-    app.all('/show', function (request, response) {
-        handlers.show(request, response);
-    });
+  app.all('/', function(request, response) {
+    handlers.welcome(request, response);
+  });
+  app.all('/upload', function(request, response, next) {
+    handlers.upload(request, response, next);
+  });
+  app.all('/show', function(request, response) {
+    handlers.show(request, response);
+  });
 
-    app.use(express.static('./templates'));
+  app.use(express.static('./templates'));
 
-    /* function onRequest(request, response) {
-        console.log("Odebrano zapytanie.".green);
-        console.log("Zapytanie " + request.url + " odebrane.");
-
-        response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-
-        switch (request.url) {
-            case '/':
-            case '/start':
-                handlers.welcome(request, response);
-                break;
-            case '/upload':
-                handlers.upload(request, response);
-                break;
-            case '/show':
-                handlers.show(request, response);
-                break;
-            case '/style.css':
-                handlers.style(request, response);
-                break;
-            default:
-                handlers.error(request, response);
-                break;
-        }
-    } */
-
-var server = app.listen(port, function() {
+  var server = app.listen(port, function() {
     console.log('Serwer nasłuchuje na http://localhost:' + port);
-});
+  });
 
-console.log("Uruchomiono serwer na porcie: ".green + port + '!'.green);
-};
+  console.log('Uruchomiono serwer na porcie: '.green + port + '!'.green);
+}
 
 exports.start = start;
